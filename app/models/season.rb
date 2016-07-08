c5e5b4089625a12ccdc75a73989d27b9c9517a01
Max Kohl <maxkohl810@gmail.com>
2016-07-08 18:09:59 -0400

    Calculate standings for a single season

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,4 +2,45 @@ class Season < ActiveRecord::Base
 	belongs_to :league
 	has_one :schedule
 	has_many :teams
+	has_many :matchups
+
+	def standings
+		standings_hash = {}
+
+		matchups.each do |matchup|
+			next if matchup.week > 21
+
+			result_one = matchup.team_results.first
+			result_two = matchup.team_results.last
+
+			[result_one, result_two].each do |result|
+				standings_hash[result.team_id] = { wins: 0, losses: 0, ties: 0} unless standings_hash.has_key?(result.team_id)
+			end
+
+			if result_one.points > result_two.points
+				standings_hash[result_one.team_id][:wins] += 1
+				standings_hash[result_two.team_id][:losses] += 1
+			elsif result_one.points < result_two.points
+				standings_hash[result_one.team_id][:losses] += 1
+				standings_hash[result_two.team_id][:wins] += 1
+			else
+				standings_hash[result_one.team_id][:ties] += 1
+				standings_hash[result_two.team_id][:ties] += 1
+			end
+		end
+
+		standings_hash
+	end
+
+	def standings_paired_to_remote_ids
+		paired_hash = {}
+
+		standings.each do |team_id, standings|
+			team = Team.find_by season: self, id: team_id
+
+			paired_hash[team.remote_id] = standings
+		end
+
+		paired_hash
+	end
 end
